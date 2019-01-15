f385e460a126e7586b6833588d338708d9650ac2
SeoulStore <stayfoolish24@gmail.com>
2019-01-15 18:36:52 +0900

    kakao login

--- App.js
+++ App.js
@@ -6,25 +6,38 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {LoginButton} from 'react-native-fbsdk'
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View, Button, Alert } from 'react-native'
+import { LoginButton } from 'react-native-fbsdk'
+import RNKakaoLogins from 'react-native-kakao-logins'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    'Shake or press menu button for dev menu'
+})
 
-type Props = {};
+type Props = {}
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <LoginButton />
+        <Button
+          onPress={() =>
+            RNKakaoLogins.login((err, result) => {
+              if (err) {
+                Alert.alert('error', err)
+                return
+              }
+              Alert.alert('result', '로그인성공')
+            })
+          }
+          title="kakaoLogin"
+        />
       </View>
-    );
+    )
   }
 }
 
@@ -33,16 +46,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
